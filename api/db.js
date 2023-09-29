9d0e97f06e357dbd57e519ed52e55096b96af7d8
ahmedkhederali <ahhmeadali1256@gmail.com>
2023-09-30 01:17:13 +0300

    first commit

---
+++ api/db.js
@@ -0,0 +1,10 @@
+import mysql from "mysql";
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const db = mysql.createConnection({
+  host:"localhost",
+  user:"root",
+  password: process.env.DB_KEY,
+  database:"blog"
+})
